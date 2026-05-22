23c72521574abf154338af32dc833089d1ca919a
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-05-22 18:04:55 +0200

    trying to figure out js and api fetch for search bar and result display

--- src/index.js
+++ src/index.js
@@ -2,6 +2,36 @@ const getMovieBtn = document.querySelector('#getMovieBtn');
 const dialogBox = document.querySelector('#searchDialog');
 const closeDialog = document.querySelector('#closeDialogBtn');
 
+
+// From the Tmdb for fetching movies
+    const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOWFkOTZkYjVhYzczMjE0YWJmYzdkYzRjNWM4MmUyOCIsIm5iZiI6MTc3OTQ1ODM4Ni4zNzQsInN1YiI6IjZhMTA2MTUyN2I4NDQ5MTdmZTU2M2M0MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l42fW_AE4SKNtCy-Hefa-Z-n7ninsr6THrDU2XNNfP4'
+  }
+};
+
+
+getMovieBtn.addEventListener('click',(e) => {
+    e.preventDefault();
+
+
+
+fetch('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1', options)
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+   dialogBox.showModal();
+});
+
+
+closeDialog.addEventListener('click', () => {
+    dialogBox.close();
+});
+
+
 // const searchResult = async () => {
 //     const res = await fetch(url); //add TmDB url for fetching movies by title
 
@@ -11,21 +41,3 @@ const closeDialog = document.querySelector('#closeDialogBtn');
 
 //     return data
 // };
-
-getMovieBtn.addEventListener('click',(e) => {
-    e.preventDefault();
-    dialogBox.showModal();
-});
-
-closeDialog.addEventListener('click', () => {
-    dialogBox.close();
-});
-
-// getMovieBtn.addEventListener('submit', async () => {
-//     try {
-//         const allMovies = await searchResult();
-//         // Dialog box for the result??
-//     } catch(error) {
-//         alert(error.message);
-//     }
-// });
